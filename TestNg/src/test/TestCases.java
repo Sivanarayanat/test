3ef5278d4248ac55d37929938480759f10b386ed
Sivanarayana <thamminenisn@gmail.com>
2024-10-16 21:49:07 +0530

    pom updated

---
+++ TestNg/src/test/TestCases.java
@@ -0,0 +1,35 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class TestCases {
+
+
+	@Test
+	public void test1() {
+
+		System.out.println("This is test 1");
+
+	}
+
+	@Test
+	public void test2() {
+
+		System.out.println("This is test 2");
+
+	}
+	
+	@Test
+	public void test3() {
+
+		System.out.println("This is test 3");
+
+	}
+	@Test
+	public void test4() {
+
+		System.out.println("This is test 4");
+
+	}
+
+}
